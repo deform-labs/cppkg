26a887b84a7011b5f6342060262eb79913da73af
YJDEV <sihamelmachhouri@gmail.com>
2026-05-17 22:13:12 +0200

    Update git_service.cpp

--- cppkg/src/services/git/git_service.cpp
+++ cppkg/src/services/git/git_service.cpp
@@ -4,14 +4,14 @@
 #include <filesystem>
 #include <iostream>
 
-/// Namespace alias for std::filesystem
+/// aliases here aliases there, are we ever going to stop using aliases? 
 namespace fs = std::filesystem;
 
-/// Constructs a GitService instance, optionally using HTTPS for all operations
+/// wooah https? 
 GitService::GitService(bool use_https)
     : https_mode_(use_https) {}
 
-/// Returns the URL for the given author and repo, using HTTPS if enabled
+/// i high-key dunno what most of this codr does i just trust it
 std::string GitService::build_url(const std::string& author, const std::string& repo, bool https) const {
     if (https || https_mode_) {
         return "https://github.com/" + author + "/" + repo + ".git";
@@ -19,12 +19,12 @@ std::string GitService::build_url(const std::string& author, const std::string& 
     return "git@github.com:" + author + "/" + repo + ".git";
 }
 
-/// Returns the path to the cloned repository
+/// get dat path
 std::string GitService::dep_path(const std::string& repo, const std::string& base) {
     return base + "/" + repo;
 }
 
-/// Clones the repository for the given author and repo, using the specified tag if provided
+/// clone that ass
 bool GitService::clone(const std::string& author, const std::string& repo, const std::string& tag) {
     std::string dest = dep_path(repo, "target/deps");
 
@@ -79,7 +79,7 @@ bool GitService::validate_repo(const std::string& author, const std::string& rep
     std::string cmd = "git ls-remote " + url + " HEAD";
     int rc = shell_.run_quiet(cmd);
 
-    // If SSH failed and we weren't already using HTTPS, try HTTPS fallback
+    // if dumb ass ssh fails lowk switch to https
     if (rc != 0 && !https_mode_) {
         url = build_url(author, repo, true);
         cmd = "git ls-remote " + url + " HEAD";
@@ -89,7 +89,8 @@ bool GitService::validate_repo(const std::string& author, const std::string& rep
     return rc == 0;
 }
 
-/// Commits the changes with the given message
+/// for beginners this saves the changes to github
+/// genuinely tho if you used git befoe you should know this
 bool GitService::commit(const std::string& message) {
     if (!shell_.command_exists("git")) {
         std::cout << Color::green << "Git not found. Please install Git: https://git-scm.com/" << Color::reset << std::endl;
@@ -100,7 +101,7 @@ bool GitService::commit(const std::string& message) {
     return rc == 0;
 }
 
-/// Pushes the changes to the remote repository
+/// cmon girl! Push!
 bool GitService::push() {
     if (!shell_.command_exists("git")) {
         return false;
